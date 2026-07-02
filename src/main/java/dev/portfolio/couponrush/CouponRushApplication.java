d64316edc09247c2d3e786eed5b42d83db50f457
seo-ju-yeon <wndus6110@naver.com>
2026-07-02 19:16:29 +0900

    Initial commit

---
+++ src/main/java/dev/portfolio/couponrush/CouponRushApplication.java
@@ -0,0 +1,13 @@
+package dev.portfolio.couponrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponRushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponRushApplication.class, args);
+    }
+
+}
